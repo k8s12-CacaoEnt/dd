773f225f1cfa36d7a21cd113f1ffa6b0464e9ece
hyewone <hyeneeOh@gamil.com>
2023-07-08 22:32:41 +0900

    save cookie on browser

--- src/pages/AuthenticationPage.jsx
+++ src/pages/AuthenticationPage.jsx
@@ -33,7 +33,11 @@ export async function action({request}){
     },
     body: JSON.stringify(authData)
   })
-  if (response.status == 422 || response.status == 401){
+
+  if (response.status === 200) {
+    const cookies = response.headers.get('Set-Cookie');
+    document.cookie = cookies;
+  }else if (response.status == 422 || response.status == 401){
     return response;
   }
   console.log(response);
